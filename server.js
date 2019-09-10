90f17035b06d80b80f60de61823794a537dbef5d
Tek Loon <tekloon.1991@gmail.com>
2019-09-10 22:28:05 +0800

    Initial Commit to Setup connection with MongoDB

---
+++ server.js
@@ -0,0 +1,21 @@
+// Import config from .env file
+require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const port = 3000
+
+// Init MongoDB Connection
+var db = mongoose.connection;
+db.on('error', () => {
+    console.error('Error Connection to MongoDB')
+});
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+mongoose.connect(process.env.mongodb_uri, {useNewUrlParser: true, authSource: 'admin', ssl: true});
+
+
+app.get('/', (req, res) => res.send('Welcome to Google Flight App!'))
+
+app.listen(port, () => console.log(`Google Flight App listening on port ${port}!`))
